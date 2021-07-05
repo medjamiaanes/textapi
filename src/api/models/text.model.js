db476132248957b71406ba66485d77c8d118f2a1
anesmeister <medjamia.anes@gmail.com>
2021-07-05 22:50:53 +0100

    set versionKey to false

--- src/api/models/text.model.js
+++ src/api/models/text.model.js
@@ -1,11 +1,14 @@
 import mongoose from 'mongoose'
 
-const schema = new mongoose.Schema({
-  translation: {
-    ar: { type: String, required: true },
-    fr: { type: String, required: true },
-    en: { type: String, required: true },
+const schema = new mongoose.Schema(
+  {
+    translation: {
+      ar: { type: String, required: true },
+      fr: { type: String, required: true },
+      en: { type: String, required: true },
+    },
   },
-})
+  { versionKey: false },
+)
 
 export default mongoose.model('Text', schema)
